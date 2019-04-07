2f8ba2470ab43e2542007c29de045188f7b274c9
TrungPhuNA <phupt.humg.94@gmail.com>
2019-04-07 16:09:51 +0700

    fix css

--- Modules/Admin/Resources/views/article/form.blade.php
+++ Modules/Admin/Resources/views/article/form.blade.php
@@ -23,7 +23,7 @@
             </div>
             <div class="form-group">
                 <label for="name">Nội dung:</label>
-                <textarea name="a_content" class="form-control" id="" cols="30" rows="3" placeholder="Nội dung">{{ old('a_content',isset($article->a_content) ? $article->a_content : '') }}</textarea>
+                <textarea name="a_content" class="form-control" id="a_content" cols="30" rows="3" placeholder="Nội dung">{{ old('a_content',isset($article->a_content) ? $article->a_content : '') }}</textarea>
                 @if($errors->has('a_content'))
                     <span class="error-text">
                         {{$errors->first('a_content')}}
@@ -49,3 +49,10 @@
     </div>
 
 </form>
+
+@section('script')
+    <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+    <script>
+		CKEDITOR.replace( 'a_content' );
+    </script>
+@stop
