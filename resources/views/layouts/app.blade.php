211349cf474377f17c40620c6afc0b510edf290e
TrungPhuNA <phupt.humg.94@gmail.com>
2019-04-12 01:43:06 +0700

    add chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -116,9 +116,19 @@
 @endif
 
 @yield('content')
-<div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="300" data-height="500"></div>
-
-<script src="https://sp.zalo.me/plugins/sdk.js"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+	var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+	(function(){
+		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+		s1.async=true;
+		s1.src='https://embed.tawk.to/5caf8a36c1fe2560f3fe6920/default';
+		s1.charset='UTF-8';
+		s1.setAttribute('crossorigin','*');
+		s0.parentNode.insertBefore(s1,s0);
+	})();
+</script>
+<!--End of Tawk.to Script-->
 <!-- FOOTER START -->
 @include('components.footer')
 <!-- FOOTER END -->
