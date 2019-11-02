c060e1bc5392f84b01033cb308d696a000b483e3
TrungPhuNA <phupt.humg.94@gmail.com>
2019-11-02 16:59:23 +0700

    fix bug cart

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -121,7 +121,6 @@ class ShoppingCartController extends FrontendController
 	public function updateShoppingCart(Request $request, $id)
 	{
 		\Cart::instance('cart')->update($id, ['qty' => $request->qty]);
-		dd($request->all());
 		return redirect()->back()->with('success','Cập nhật thành công');
 	}
 	
